de7ccbf1814b8cf929b9dba43811b96009dfe95e
Mavi <mariapimenta2607@gmail.com>
2024-11-22 09:16:39 -0300

    fix again

--- src/App.jsx
+++ src/App.jsx
@@ -94,15 +94,15 @@ function App() {
         </div>
       </div>
       <div className="flex justify-center w-full">
-        <form className="max-w-md mx-auto text-black" onSubmit={handleSubmit}>
+        <form className="max-w-md mx-auto " onSubmit={handleSubmit}>
           <div className="relative z-0 w-full mb-5 group">
-            <input onChange={(e) => {setName(e.target.value)}} type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Name " required />
+            <input onChange={(e) => {setName(e.target.value)}} type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Name " required />
           </div>
           <div className="relative z-0 w-full mb-5 group">
-            <input onChange={(e) => {setLastname(e.target.value)}} type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Lastname " required />
+            <input onChange={(e) => {setLastname(e.target.value)}} type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Lastname " required />
           </div>
           <div className="relative z-0 w-full mb-5 group">
-            <input onChange={(e) => {setSalary(e.target.value)}} type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Salary " required />
+            <input onChange={(e) => {setSalary(e.target.value)}} type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Salary " required />
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
